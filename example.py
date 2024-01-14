c8078134a87c29a8c2751b3ea5f13f4492528aee
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-01-14 23:38:26 +0100

    updated base examples

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tropy.learn import fit_hyperplane
-from tropy.graph import init_ax, plot_points, plot_hyperplane
+from tropy.svm import TropicalSVM
+from tropy.graph import init_ax, plot_classes, plot_hyperplane, set_title, get_ignored
 from tropy.utils import apply_noise
 
 if __name__ == "__main__":
@@ -17,33 +17,32 @@ if __name__ == "__main__":
     dtype=float,
   ).T
   Cminus = np.array([[10, -5, -5], [-3, 6, -3], [-2, -5, 7]], dtype=float).T
-  apply_noise(Cplus)
-  apply_noise(Cminus)
-
+  apply_noise(Cplus, 0.3)
+  apply_noise(Cminus, 0.3)
   Cplus_sep, Cminus_sep = Cplus.copy(), Cminus.copy()
   Cplus_sep[1, :] -= 10
   Cminus_sep[1, :] += 10
 
-  L = 15  # Plot scale factor
+  # Fitting tropical SVM
+  model1, model2 = TropicalSVM(), TropicalSVM()
+  model1.fit([Cplus_sep, Cminus_sep])
+  model2.fit([Cplus, Cminus])
 
+  L = 15  # Plot scale factor
   fig = plt.figure()
-  ax0 = init_ax(fig, 221, L)
-  ax1 = init_ax(fig, 222, L)
-  ax2 = init_ax(fig, 223, L)
-  ax3 = init_ax(fig, 224, L)
+  ax1 = init_ax(fig, 121, L)
+  ax2 = init_ax(fig, 122, L)
 
   # Example, separable case
-  ax0.set_title("Raw separable")
-  plot_points(ax0, Cplus_sep, L, "r", marker="o")
-  plot_points(ax0, Cminus_sep, L, "b", linestyle="dashed", marker="v")
-  plot_hyperplane(ax1, "Inrad method, separable",
-                  *fit_hyperplane(Cplus_sep, Cminus_sep), L)
+  plot_classes(ax1, [Cplus_sep, Cminus_sep], L, show_lines=True)
+  ignored_branch = get_ignored(Cplus_sep, Cminus_sep, model1.apex)
+  plot_hyperplane(ax1, model1.apex, model1.eigval, L, ignored_branch)
+  set_title(ax1, "Inrad method, separable", model1.apex, model1.eigval)
 
   # Example, inseparable case
-  ax2.set_title("Raw inseparable")
-  plot_points(ax2, Cplus, L, "r", marker="o")
-  plot_points(ax2, Cminus, L, "b", linestyle="dashed", marker="v")
-  plot_hyperplane(ax3, "Inrad method, inseparable",
-                  *fit_hyperplane(Cplus, Cminus), L)
+  plot_classes(ax2, [Cplus, Cminus], L, show_lines=True)
+  ignored_branch = get_ignored(Cplus, Cminus, model2.apex)
+  plot_hyperplane(ax2, model2.apex, model2.eigval, L, ignored_branch)
+  set_title(ax2, "Inrad method, inseparable", model2.apex, model2.eigval)
 
   plt.show()
