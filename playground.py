7ca5a8f776c5997350b4906203596b263c2e1ba7
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-02-06 16:54:21 +0100

    new toy data + margin when s>1 + grid mode

--- playground.py
+++ playground.py
@@ -17,19 +17,17 @@ def main(args):
   DATASET = args.dataset
   log_linear_beta = args.beta
   show_ball = args.show_ball
-  if show_ball:
-    assert degree == 1
-  grid_mode = False
+  grid_mode = args.grid_mode
 
   def toy_gaussian(center: list):
-    return apply_noise(np.array([center] * 10, dtype=float).T, mu=0.1, seed=None)
+    return apply_noise(np.array([center] * 10, dtype=float).T, mu=0.3, seed=None)
 
   centers_max_plus = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]
   centers_min_plus = [[0, 1, 1], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
   centers = centers_max_plus if not 'reverse' in DATASET else centers_min_plus
   if 'bintoy' in DATASET:
     native_tropical = True
-    Xplus, Xminus = build_toy_dataset(1000, 3, 2, noise=True)
+    Xplus, Xminus = build_toy_dataset(80, 3, 2, noise=True)
     if 'separated' in DATASET:
       Xplus[2, :] -= 5
       Xminus[2, :] += 5
@@ -39,13 +37,22 @@ def main(args):
     apply_noise(Xplus, seed=2024, mu=0.2)
     apply_noise(Xminus, seed=2024, mu=0.2)
     data_classes = [Xplus, Xminus]
+  if 'circular' in DATASET:
+    native_tropical = True
+    C = np.random.normal([[0, 0, 0]]*100, 10, (100, 3)).T
+    positive_mask = np.max(C, axis=0) - np.min(C, axis=0) > 15
+    negative_mask = ~positive_mask
+    C[:, negative_mask] /= 3
+    Xplus = C[:, positive_mask]
+    Xminus = C[:, negative_mask]
+    data_classes = [Xplus, Xminus]
   elif 'toy' in DATASET:
     native_tropical = True
     data_classes = [np.array([c]).T if 'centers' in DATASET else toy_gaussian(c) for c in centers]
   elif 'iris' in DATASET:
     native_tropical = False
     base_df = pd.read_csv('./notebooks/data/IRIS.csv')
-    df = base_df.loc[:, ['sepal_length', 'petal_length']]
+    df = base_df.loc[:, ['sepal_width', 'petal_width']]
     classes = ["Iris-virginica", "Iris-versicolor"]
     if not 'binary' in DATASET:
       classes.insert(0, "Iris-setosa")
@@ -72,11 +79,11 @@ def main(args):
       method = f'linear SVM on log paper, $\\beta = {log_linear_beta}$'
     else:
       method = 'mean payoff games'
-    if show_ball:
+    if show_ball and degree == 1 and model._eigval < 0:
       plot_ball(ax, model._apex, np.abs(model._eigval))
     if not save:
       ax.set_title(f'$deg = {degree}$, using {method}', fontsize='small', loc='left')
-    plot_classes(ax, model._data_classes, L=10)
+    plot_classes(ax, model._data_classes, L=10, show_balls=(0 if degree==1 or show_ball == False or model._eigval >= 0 else np.abs(model._eigval)/degree))
     plot_polynomial_hypersurface_3d(ax, model._monomials, model._coeffs, L=10, sector_indicator=model._sector_indicator)
 
   else:
@@ -87,10 +94,10 @@ def main(args):
       model.fit(data, degree, tropical_data=native_tropical, log_linear_beta=log_linear_beta)
 
       ax = init_ax(fig, [2, 2, degree], L=10)
-      plot_classes(ax, model._data_classes, L=10)
-      plot_polynomial_hypersurface_3d(ax, model._monomials, model._coeffs, L=25, sector_indicator=model._sector_indicator)
+      plot_classes(ax, model._data_classes, L=10, show_balls=(0 if degree==1 or show_ball == False or model._eigval >= 0 else np.abs(model._eigval)/degree))
+      plot_polynomial_hypersurface_3d(ax, model._monomials, model._coeffs, L=10, sector_indicator=model._sector_indicator)
       ax.set_title(f'$deg = {degree}$', fontsize='small', loc='left')
-      if show_ball:
+      if show_ball and degree == 1 and model._eigval < 0:
         plot_ball(ax, model._apex, np.abs(model._eigval))
 
   if save:
@@ -101,11 +108,12 @@ def main(args):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Fitting and plotting tropical piecewise linear classifiers on 3D datasets")
-  parser.add_argument("dataset", type=str, choices=['iris', 'iris-binary', 'toy', 'toy-centers', 'toy-reverse', 'toy-centers-reverse', 'bintoy', 'bintoy-separated', 'bintoy-mixed'], help="Dataset to classify")
+  parser.add_argument("dataset", type=str, choices=['iris', 'iris-binary', 'toy', 'toy-centers', 'toy-reverse', 'toy-centers-reverse', 'bintoy', 'bintoy-separated', 'bintoy-mixed', 'circular'], help="Dataset to classify")
   parser.add_argument("degree", nargs='?', type=int, default=1, help="Degree of tropical polynomial")
   parser.add_argument("-s", "--save", action="store_true", help="Save the figure (.PGF)")
   parser.add_argument("--beta", type=float, default=None, help="If specified, Beta value for using 'linear SVM on log paper' trick")
   parser.add_argument("--show-ball", action="store_true", help="Show the Hilbert ball corresponding to inner radius or margin")
+  parser.add_argument("--grid-mode", action="store_true", help="Show a grid of multiple tropical polynomials")
 
   if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
